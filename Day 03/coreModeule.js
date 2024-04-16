145961bb6c1cc4c0370c2cc4a84de5e20392c223
Samee <samiuzzama619@gmail.com>
2024-04-16 23:27:40 +0530

    understand core module

---
+++ Day 03/coreModeule.js
@@ -0,0 +1,7 @@
+console.log('hello')
+// fs = file system
+// Buffer : used for data processing
+// HTTP: used for api call
+
+const fs = require('fs')
+fs.writeFileSync('hello.txt', 'samee uz zama')
